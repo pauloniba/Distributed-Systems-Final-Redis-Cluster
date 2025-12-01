b3554aa2c4fbee32bc638d13cb4f41cf13c51388
e-black01 <157631250+e-black01@users.noreply.github.com>
2025-12-01 14:45:48 -0600

    Update update.js

--- api/update.js
+++ api/update.js
@@ -1,1 +1,12 @@
+import redis from "../redis/redisClient.js";
 
+export async function updateData(req, res) {
+  try {
+    const { id, name, email, score } = req.body;
+    if (!id) {
+    return res.status(400).json({ error: "id required" });
+      
+  }
+} catch (err){
+        return res.status(500).json({error: "Server error", details: err});
+}}
