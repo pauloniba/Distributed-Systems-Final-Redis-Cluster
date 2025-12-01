7d39bb4875ff0958e194316e927715d506cfc015
e-black01 <157631250+e-black01@users.noreply.github.com>
2025-12-01 14:33:41 -0600

    Update delete.js

--- delete.js
+++ delete.js
@@ -1,1 +1,7 @@
-
+import redis from "../redis/redisClient.js";
+export async function deleteData(req,res){
+ try{
+    
+    } catch (err){
+        return res.status(500).json({error: "Server error", details: err});
+}}
