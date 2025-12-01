55420d4f2527cad930c067819324167f08169c04
e-black01 <157631250+e-black01@users.noreply.github.com>
2025-12-01 15:02:27 -0600

    Update update.js

--- api/update.js
+++ api/update.js
@@ -4,13 +4,21 @@ export async function updateData(req, res) {
   try {
     const { id, name, email, score } = req.body;
     if (!id) {
-    return res.status(400).json({ error: "id required" });
-  }
+      return res.status(400).json({ error: "id required" });
+    }
+    
+    const exists = await redis.exists(key);
+    if (!exists) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    
     const updates = {};
     if (name) updates.name = name;
     if (email) updates.email = email;
     if (score) updates.score = score;
     
-} catch (err){
+  }
+  catch (err) {
         return res.status(500).json({error: "Server error", details: err});
-}}
+  }
+}
